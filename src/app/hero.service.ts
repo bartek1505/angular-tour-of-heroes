188a8adf603ebb3a01a22e5b8a5b84c97a8e8132
Bartek <Bartek@LAPTOP-1H508GIL>
2019-06-07 17:53:07 +0200

    Services - Messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 
 @Injectable({
@@ -8,11 +10,13 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor() { }
-  getHeroes(): Hero[] {
-    return HEROES;
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 
- 
 }
+
 
